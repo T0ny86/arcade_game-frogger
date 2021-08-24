d09d8ab819626a8505c46065afe21eedbef7726e
Tanous Atallah <tanous.atallah@gmail.com>
2021-08-24 12:57:32 +0200

    fix the relative path to fixed one - test 4

--- scripts/app.js
+++ scripts/app.js
@@ -45,30 +45,34 @@ const logsArray = [];
 //images :
 const background_lvl2 = new Image();
 // background_lvl2.src = "../asset/background_lvl2.png";
-background_lvl2.src = "//arcade_game-frogger/asset/background_lvl2.png";
+background_lvl2.src = "./arcade_game-frogger/asset/background_lvl2.png";
 
 const grass = new Image();
 // grass.src = "../asset/grass.png";
-grass.src = ".//arcade_game-frogger/asset/grass.png";
+grass.src = "arcade_game-frogger/asset/grass.png";
 
 const collisionImg = new Image();
 collisionImg.src = "arcade_game-frogger/asset/collisions.png";
 
 const turtle = new Image();
-turtle.src = "../asset/turtles.png";
+// turtle.src = "../asset/turtles.png";
+turtle.src = "arcade_game-frogger/asset/turtles.png";
 
 const log = new Image();
-log.src = "../asset/log.png";
+// log.src = "../asset/log.png";
+log.src = "arcade_game-frogger/asset/log.png";
 
 const car = new Image();
-car.src = "../asset/cars.png";
+// car.src = "../asset/cars.png";
+car.src = "arcade_game-frogger/asset/cars.png";
 
 let numberOfCars = 3;
 
 const frogSprite = new Image();
-frogSprite.src = "../asset/frog_spritesheet.png";
+// frogSprite.src = "../asset/frog_spritesheet.png";
+frogSprite.src = "arcade_game-frogger/asset/frog_spritesheet.png";
 
 // sound effects
 
 const jumpSound = new Audio();
-jumpSound.src = "../asset/jump.wav";
+jumpSound.src = "arcade_game-frogger/asset/jump.wav";
