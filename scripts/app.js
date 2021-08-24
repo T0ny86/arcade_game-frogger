48dc7864cd17d8ac08bf7c1f8e78e8aa195f554d
Tanous Atallah <tanous.atallah@gmail.com>
2021-08-24 12:48:26 +0200

    fix the relative path to fixed one - test 2

--- scripts/app.js
+++ scripts/app.js
@@ -45,7 +45,7 @@ const logsArray = [];
 //images :
 const background_lvl2 = new Image();
 // background_lvl2.src = "../asset/background_lvl2.png";
-background_lvl2.src = "/arcade_game-frogger/asset/background_lvl2.png";
+background_lvl2.src = "./arcade_game-frogger/asset/background_lvl2.png";
 
 const grass = new Image();
 // grass.src = "../asset/grass.png";
